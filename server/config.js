67a71ce8659fd838f69490a9dcc7953d347453a3
X4M7 <1415808154@qq.com>
2026-03-13 11:50:31 +0800

    更新.gitignore文件，添加.trae/目录忽略规则

--- server/config.js
+++ server/config.js
@@ -1,10 +1,9 @@
 // 讯飞API配置
-// 请替换为您的实际API密钥
 module.exports = {
   xfyun: {
-    appId: '您的AppID',
-    apiKey: '您的APIKey',
-    apiSecret: '您的APISecret',
-    url: 'wss://iat-api.xfyun.cn/v2/iat'
+    appId: '69f25a32',
+    apiKey: '1278a7238d7c2fa06aeceb2d7f11dc50',
+    apiSecret: 'YzYxYTA4Mjc3Mzk4YjM4ZjdiZDQxN2E2',
+    url: 'wss://office-api-ast-dx.iflyaisol.com/ast/communicate/v1'
   }
 };
