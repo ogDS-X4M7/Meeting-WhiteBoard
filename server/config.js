500381d9ebcc62f5a74f3cf90087afd2ce1cc63c
X4M7 <1415808154@qq.com>
2026-03-14 17:19:50 +0800

    移除config.js

--- server/config.js
+++
@@ -1,9 +0,0 @@
-// 讯飞API配置
-module.exports = {
-  xfyun: {
-    appId: '69f25a32',
-    apiKey: '1278a7238d7c2fa06aeceb2d7f11dc50',
-    apiSecret: 'YzYxYTA4Mjc3Mzk4YjM4ZjdiZDQxN2E2',
-    url: 'wss://office-api-ast-dx.iflyaisol.com/ast/communicate/v1'
-  }
-};
